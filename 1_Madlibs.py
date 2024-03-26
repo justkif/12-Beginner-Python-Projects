4693441be23bc239296694f1a8308d7df9281b09
kiiiiiiiiif <kyle.vuanh@gmail.com>
2024-03-26 23:41:17 +0700

    ok

---
+++ 1_Madlibs.py
@@ -0,0 +1,9 @@
+print("This Madlibs is inspired by Mad Lib Theater with Benedict Cumberbatch on The Tonight Show Starring Jimmy Fallon.")
+
+male_name = input("Male name: ")
+fav_teacher = input("Name of your favorite teacher: ")
+exclamation = input("Exclamation: ")
+number = input("Number: ")
+plural_obj = input("Plural object: ")
+store_name = input("Store name: ")
+body_part = input("Body part: ")
